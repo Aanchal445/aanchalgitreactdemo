41a83022f75ae6ba7d415e5b236617ca76a8b9c0
aanchal <aanchalarora7526@gmail.com>
2024-06-06 00:44:29 -0400

    NewComponent added to the project

---
+++ reactaanchal/src/newfeature.jsx
@@ -0,0 +1,13 @@
+// src/NewComponent.jsx
+import React from 'react';
+
+const NewComponent = () => {
+  return (
+    <div>
+      <h1>New Component</h1>
+      <p>This is a new component added in the feature branch.</p>
+    </div>
+  );
+};
+
+export default NewComponent;
